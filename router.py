34d010c89c6e5408751dd68241f36ec6d5faf002
vietbq <vietbq@nal.vn>
2019-06-20 19:28:38 +0700

    Build 19:26

--- router.py
+++ router.py
@@ -13,7 +13,7 @@ cors = CORS(app)
 
 @app.route("/")
 def index():
-    return "Welcome to Wisdom Seeker!"
+    return "Welcome to Wisdom Seeker! 19:26"
 
 @app.route('/api/translate', endpoint='translate', methods=['POST'])
 def translate():
@@ -23,15 +23,14 @@ def translate():
     response = generate_response(0, output)
     return response
 
-def generate_response(code=0, message='', output=None):
-    response = {'code': code, 'message': message, 'output': output}
+def generate_response(code=0, output=None):
+    response = {'code': code, 'message': '', 'output': output}
     res = json.dumps(response)
     response = make_response(res)
     response.headers['Content-Type'] = 'application/json'
     return response
 
 if __name__ == '__main__':
-    translation('Hi')
     port = int(os.environ.get("PORT", 5000))
     print("Starting app on port %d" % port)
     app.run(threaded=True, debug=False, port=port,host = '0.0.0.0')
